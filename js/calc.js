149cf7dcaeb767e82ba2da845c98e779874452b2
mrtechexperts <65217075+mrtechexperts@users.noreply.github.com>
2020-10-13 12:52:15 +0600

    clac added

---
+++ js/calc.js
@@ -0,0 +1,30 @@
+function calcul() {
+    document.getElementById("ex").innerHTML = "";
+    var fst = document.getElementById("1st").value;
+    var op = document.getElementById("op").value;
+    var scnd = document.getElementById("2nd").value;
+    var result;
+    fst = Number(fst);
+    scnd = Number(scnd);
+    if (op == "+") {
+        result = fst + scnd;
+    } else if (op == "-") {
+        result = fst - scnd;
+    } else if (op == "x") {
+        result = fst * scnd;
+    } else if (op == "/") {
+        result = fst / scnd;
+    } else if (op == "%") {
+        exResult = fst % scnd;
+        result = Math.floor(fst / scnd);
+        document.getElementById("ex").innerHTML = "Remiander is: " + exResult;
+    } else if (op == "^") {
+        result = fst ** scnd;
+    } else {
+        alert("invalid");
+        location.reload();
+    }
+    document.getElementById("ans").innerHTML = "Answer is " + result;
+    console.log(result);
+
+}
